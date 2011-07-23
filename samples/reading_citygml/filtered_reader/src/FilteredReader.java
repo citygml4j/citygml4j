ed52028acf5fe2bc833dba7de9175fcdb0615d54
Claus Nagel <cnagel@virtualcitysystems.de>
2011-07-23 11:01:33 +0000

    added sample program demonstrating the use of a filtered CityGML reader

---
+++ samples/reading_citygml/filtered_reader/src/FilteredReader.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.citygml4j.CityGMLContext;
+import org.citygml4j.builder.CityGMLBuilder;
+import org.citygml4j.model.citygml.CityGMLClass;
+import org.citygml4j.model.citygml.transportation.Road;
+import org.citygml4j.xml.io.CityGMLInputFactory;
+import org.citygml4j.xml.io.reader.CityGMLInputFilter;
+import org.citygml4j.xml.io.reader.CityGMLReader;
+import org.citygml4j.xml.io.reader.FeatureReadMode;
+
+
+public class FilteredReader {
+
+	public static void main(String[] args) throws Exception {
+		SimpleDateFormat df = new SimpleDateFormat("[HH:mm:ss] "); 
+
+		System.out.println(df.format(new Date()) + "setting up citygml4j context and JAXB builder");
+		CityGMLContext ctx = new CityGMLContext();
+		CityGMLBuilder builder = ctx.createCityGMLBuilder();
+
+		System.out.println(df.format(new Date()) + "reading only roads from CityGML file LOD2_CityObjectGroup_v100.xml");
+		CityGMLInputFactory in = builder.createCityGMLInputFactory();
+		in.setProperty(CityGMLInputFactory.FEATURE_READ_MODE, FeatureReadMode.SPLIT_PER_FEATURE);
+
+		CityGMLReader reader = in.createCityGMLReader(new File("../../datasets/LOD2_CityObjectGroup_v100.xml"));
+		reader = in.createFilteredCityGMLReader(reader, new CityGMLInputFilter() {
+
+			// return true if you want to consume the CityGML feature
+			// of the given CityGMLClass type, false otherwise
+			public boolean accept(CityGMLClass type) {
+				return type == CityGMLClass.ROAD;
+			}
+			
+		});
+
+		System.out.println(df.format(new Date()) + "printing road features");
+		while (reader.hasNextFeature()) {
+			Road road = (Road)reader.nextFeature();
+			System.out.println(df.format(new Date()) + "found Road with gml:id " + road.getId());	
+			
+			System.out.println(df.format(new Date()) + "\t" + road.getTrafficArea().size() + " traffic area(s)");	
+			System.out.println(df.format(new Date()) + "\t" + road.getAuxiliaryTrafficArea().size() + " auxiliary traffic area(s)");	
+		}
+
+		reader.close();
+		System.out.println(df.format(new Date()) + "sample citygml4j application successfully finished");
+	}
+
+}
