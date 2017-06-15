71aa484694c6239c4deeac3d08703bbe9aa34d55
Claus Nagel <cnagel@virtualcitysystems.de>
2017-06-15 14:28:48 +0200

    added link between ADEModelObject and ADEModule similar to the CityGML model classes

--- src/org/citygml4j/model/citygml/ade/binding/ADEModelObject.java
+++ src/org/citygml4j/model/citygml/ade/binding/ADEModelObject.java
@@ -4,6 +4,7 @@ import org.citygml4j.model.citygml.CityGMLClass;
 import org.citygml4j.model.citygml.ade.ADEClass;
 import org.citygml4j.model.citygml.ade.ADEComponent;
 import org.citygml4j.model.common.base.ModelType;
+import org.citygml4j.model.module.ade.ADEModule;
 
 public interface ADEModelObject extends ADEComponent {
 
@@ -20,6 +21,10 @@ public interface ADEModelObject extends ADEComponent {
 	@Override
 	default ADEClass getADEClass() {
 		return ADEClass.MODEL_OBJECT;
+	}
+	
+	default ADEModule getADEModule() {
+		return null;
 	}
 	
 }
