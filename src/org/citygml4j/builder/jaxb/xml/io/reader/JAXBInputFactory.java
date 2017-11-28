0dc6d5dc80442a95b11d23fcf0f0d2d78de33af0
clausnagel <cnagel@virtualcitysystems.de>
2017-11-28 15:05:34 +0100

    code clean up

--- src/org/citygml4j/builder/jaxb/xml/io/reader/JAXBInputFactory.java
+++ src/org/citygml4j/builder/jaxb/xml/io/reader/JAXBInputFactory.java
@@ -23,25 +23,68 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
 
-import org.citygml4j.builder.jaxb.CityGMLBuilderException;
 import org.citygml4j.builder.jaxb.CityGMLBuilder;
+import org.citygml4j.builder.jaxb.CityGMLBuilderException;
+import org.citygml4j.util.gmlid.DefaultGMLIdManager;
+import org.citygml4j.util.gmlid.GMLIdManager;
 import org.citygml4j.util.internal.xml.SystemIDResolver;
-import org.citygml4j.xml.io.AbstractCityGMLInputFactory;
+import org.citygml4j.util.internal.xml.TransformerChainFactory;
+import org.citygml4j.xml.io.CityGMLInputFactory;
 import org.citygml4j.xml.io.reader.CityGMLInputFilter;
 import org.citygml4j.xml.io.reader.CityGMLReadException;
 import org.citygml4j.xml.io.reader.CityGMLReader;
+import org.citygml4j.xml.io.reader.FeatureReadMode;
+import org.citygml4j.xml.io.writer.CityGMLWriteException;
 import org.citygml4j.xml.schema.SchemaHandler;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
-public class JAXBInputFactory extends AbstractCityGMLInputFactory {
+public class JAXBInputFactory implements CityGMLInputFactory {
 	CityGMLBuilder builder;
+	private SchemaHandler schemaHandler;
+	private XMLInputFactory xmlInputFactory;
+	private GMLIdManager gmlIdManager;
+	private ValidationEventHandler validationEventHandler;
+	private TransformerChainFactory transformerChainFactory;
+
+	private FeatureReadMode featureReadMode;
+	private List<QName> excludes;
+	private List<QName> splitAtFeatureProperties;
+	private boolean keepInlineAppearance;
+	private boolean parseSchema;
+	private boolean useValidation;
+	private boolean failOnMissingADESchema;
+	private boolean supportCityGML040;
 
 	public JAXBInputFactory(CityGMLBuilder builder, SchemaHandler schemaHandler) {
-		super(schemaHandler);
 		this.builder = builder;
+		this.schemaHandler = schemaHandler;
+
+		xmlInputFactory = XMLInputFactory.newInstance();
+		xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.TRUE);
+
+		gmlIdManager = DefaultGMLIdManager.getInstance();
+		validationEventHandler = null;
+		featureReadMode = FeatureReadMode.NO_SPLIT;
+		excludes = new ArrayList<QName>();
+		splitAtFeatureProperties = new ArrayList<QName>();
+		keepInlineAppearance = true;
+		parseSchema = true;
+		useValidation = false;
+		failOnMissingADESchema = true;
+		supportCityGML040 = false;
 	}
 	
 	public JAXBInputFactory(CityGMLBuilder builder) throws CityGMLBuilderException {
@@ -128,6 +171,220 @@ public class JAXBInputFactory extends AbstractCityGMLInputFactory {
 
 		return reader;
 	}
+	
+	public XMLInputFactory getXMLInputFactory() {
+		return xmlInputFactory;
+	}
+
+	public void setXMLInputFactory(XMLInputFactory xmlInputFactory) {
+		if (xmlInputFactory == null)
+			throw new IllegalArgumentException("xml input factory may not be null.");
+
+		this.xmlInputFactory = xmlInputFactory;
+	}
+
+	public GMLIdManager getGMLIdManager() {
+		return gmlIdManager;
+	}
+
+	public void setGMLIdManager(GMLIdManager gmlIdManager) {
+		if (gmlIdManager == null)
+			throw new IllegalArgumentException("gml:id manager may not be null.");
+
+		this.gmlIdManager = gmlIdManager;
+	}
+
+	public SchemaHandler getSchemaHandler() {
+		return schemaHandler;
+	}
+
+	public void setSchemaHandler(SchemaHandler schemaHandler) {
+		if (schemaHandler == null)
+			throw new IllegalArgumentException("schema handler may not be null.");
+
+		this.schemaHandler = schemaHandler;
+	}
+
+	public ValidationEventHandler getValidationEventHandler() {
+		return validationEventHandler;
+	}
+
+	public void setValidationEventHandler(ValidationEventHandler validationEventHandler) {
+		if (validationEventHandler == null)
+			throw new IllegalArgumentException("validation event handler may not be null.");
+
+		this.validationEventHandler = validationEventHandler;
+	}
+	
+	public void setTransformationTemplates(Templates... transformationTemplates) throws CityGMLWriteException {
+		if (transformationTemplates == null)
+			throw new IllegalArgumentException("transformation templates may not be null.");
+
+		try {
+			if (transformerChainFactory == null)
+				transformerChainFactory = new TransformerChainFactory(transformationTemplates);
+			else
+				transformerChainFactory.updateTemplates(transformationTemplates);
+		} catch (TransformerConfigurationException e) {
+			throw new CityGMLWriteException("Caused by: ", e);
+		}
+	}
+
+	public Templates[] getTransformationTemplates() {
+		return transformerChainFactory == null ? null : transformerChainFactory.getTemplates();
+	}
+
+	public TransformerChainFactory getTransformerChainFactory() {
+		return transformerChainFactory;
+	}
+
+	public Object getProperty(String name) {
+		if (name == null)
+			throw new IllegalArgumentException("name may not be null.");
+
+		if (name.equals(CityGMLInputFactory.FEATURE_READ_MODE))
+			return featureReadMode;
+		if (name.equals(CityGMLInputFactory.KEEP_INLINE_APPEARANCE))
+			return keepInlineAppearance;
+		if (name.equals(CityGMLInputFactory.PARSE_SCHEMA))
+			return parseSchema;
+		if (name.equals(CityGMLInputFactory.USE_VALIDATION))
+			return useValidation;
+		if (name.equals(CityGMLInputFactory.EXCLUDE_FROM_SPLITTING))
+			return excludes;
+		if (name.equals(CityGMLInputFactory.SPLIT_AT_FEATURE_PROPERTY))
+			return splitAtFeatureProperties;
+		if (name.equals(CityGMLInputFactory.FAIL_ON_MISSING_ADE_SCHEMA))
+			return failOnMissingADESchema;
+		if (name.equals(CityGMLInputFactory.SUPPORT_CITYGML_VERSION_0_4_0))
+			return supportCityGML040;
+
+		throw new IllegalArgumentException("the property '" + name + "' is not supported.");
+	}
+
+	public void setProperty(String name, Object value) {
+		if (name == null)
+			throw new IllegalArgumentException("name may not be null.");
+
+		if (name.equals(CityGMLInputFactory.FEATURE_READ_MODE)) {
+			FeatureReadMode mode = FeatureReadMode.fromValue(value.toString());
+			if (mode != null)
+				featureReadMode = mode;
+
+			return;
+		}
+
+		if (name.equals(CityGMLInputFactory.KEEP_INLINE_APPEARANCE)) {
+			if (value instanceof Boolean)
+				keepInlineAppearance = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		if (name.equals(CityGMLInputFactory.PARSE_SCHEMA)) {
+			if (value instanceof Boolean)
+				parseSchema = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+		
+		if (name.equals(CityGMLInputFactory.USE_VALIDATION)) {
+			if (value instanceof Boolean)
+				useValidation = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		if (name.equals(CityGMLInputFactory.EXCLUDE_FROM_SPLITTING)) {
+			if (value instanceof QName){
+				excludes.add((QName)value);
+				return;
+			} 
+
+			else if (value instanceof Collection<?>) {
+				for (Object o : (Collection<?>)value) {
+					if (o instanceof QName)
+						excludes.add((QName)o);
+					else
+						throw new IllegalArgumentException("exclude must be of type java.xml.namespace.QName.");
+				}
+				
+				return;
+			}
+
+			else if (value instanceof Object[]) {
+				for (Object o : (Object[])value) {
+					if (o instanceof QName)
+						excludes.add((QName)o);
+					else
+						throw new IllegalArgumentException("exclude must be of type java.xml.namespace.QName.");
+				}
+				
+				return;
+			}
+		}
+		
+		if (name.equals(CityGMLInputFactory.SPLIT_AT_FEATURE_PROPERTY)) {
+			if (value instanceof QName) {
+				splitAtFeatureProperties.add((QName)value);
+				return;
+			}
+			
+			else if (value instanceof Collection<?>) {
+				for (Object o : (Collection<?>)value) {
+					if (o instanceof QName)
+						splitAtFeatureProperties.add((QName)o);
+					else
+						throw new IllegalArgumentException("feature property must be specified using java.xml.namespace.QName.");
+				}
+				
+				return;
+			}
+
+			else if (value instanceof Object[]) {
+				for (Object o : (Object[])value) {
+					if (o instanceof QName)
+						splitAtFeatureProperties.add((QName)o);
+					else
+						throw new IllegalArgumentException("feature property must be specified using java.xml.namespace.QName.");
+				}
+				
+				return;
+			}
+		}
+		
+		if (name.equals(CityGMLInputFactory.FAIL_ON_MISSING_ADE_SCHEMA)) {
+			if (value instanceof Boolean)
+				failOnMissingADESchema = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+		
+		if (name.equals(CityGMLInputFactory.SUPPORT_CITYGML_VERSION_0_4_0)) {
+			if (value instanceof Boolean)
+				supportCityGML040 = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		throw new IllegalArgumentException("the key-value pair '" + name + " - " + value.getClass().getName() + "' is not supported.");
+	}
+
+	public boolean registerSchemaLocation(String namespaceURI, File schemaLocation) {
+		return schemaHandler.registerSchemaLocation(namespaceURI, schemaLocation);
+	}
+
+	public void parseSchema(File schemaLocation) throws SAXException {
+		schemaHandler.parseSchema(schemaLocation);
+	}
+
+	public void parseSchema(String namespaceURI, String schemaLocation) throws SAXException {
+		schemaHandler.parseSchema(namespaceURI, schemaLocation);	
+	}
+
+	public void parseSchema(Element element) throws SAXException {
+		schemaHandler.parseSchema(element);
+	}
 
 	private URI toURI(String baseURI) {
 		URI uri = null;
@@ -140,4 +397,5 @@ public class JAXBInputFactory extends AbstractCityGMLInputFactory {
 
 		return uri;
 	}
+	
 }
--- src/org/citygml4j/builder/jaxb/xml/io/writer/JAXBOutputFactory.java
+++ src/org/citygml4j/builder/jaxb/xml/io/writer/JAXBOutputFactory.java
@@ -25,39 +25,66 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.stream.StreamResult;
 
 import org.citygml4j.builder.jaxb.CityGMLBuilder;
+import org.citygml4j.model.citygml.CityGML;
 import org.citygml4j.model.module.ModuleContext;
+import org.citygml4j.model.module.citygml.CityGMLVersion;
+import org.citygml4j.util.gmlid.DefaultGMLIdManager;
+import org.citygml4j.util.gmlid.GMLIdManager;
+import org.citygml4j.util.internal.xml.TransformerChainFactory;
 import org.citygml4j.util.xml.SAXWriter;
-import org.citygml4j.xml.io.AbstractCityGMLOutputFactory;
+import org.citygml4j.xml.io.CityGMLOutputFactory;
 import org.citygml4j.xml.io.writer.CityGMLWriteException;
 import org.citygml4j.xml.io.writer.CityGMLWriter;
 import org.citygml4j.xml.io.writer.CityModelWriter;
+import org.citygml4j.xml.io.writer.FeatureWriteMode;
 import org.citygml4j.xml.schema.SchemaHandler;
 
-public class JAXBOutputFactory extends AbstractCityGMLOutputFactory {
+public class JAXBOutputFactory implements CityGMLOutputFactory {
 	CityGMLBuilder builder;
+	private SchemaHandler schemaHandler;	
+	private ModuleContext moduleContext;
+	private GMLIdManager gmlIdManager;
+	private ValidationEventHandler validationEventHandler;
+	private TransformerChainFactory transformerChainFactory;
+
+	private FeatureWriteMode featureWriteMode;
+	private Set<Class<? extends CityGML>> excludes;
+	private boolean keepInlineAppearance;
+	private boolean splitCopy;
+	private boolean useValidation;
+	
+	public JAXBOutputFactory(CityGMLBuilder builder, ModuleContext moduleContext, SchemaHandler schemaHandler) {
+		this.builder = builder;
+		this.schemaHandler = schemaHandler;
+		this.moduleContext = moduleContext;
+
+		gmlIdManager = DefaultGMLIdManager.getInstance();
+		featureWriteMode = FeatureWriteMode.NO_SPLIT;
+		excludes = new HashSet<Class<? extends CityGML>>();
+		keepInlineAppearance = true;
+		splitCopy = true;
+	}
 	
 	public JAXBOutputFactory(CityGMLBuilder builder) {
-		super();
-		this.builder = builder;
+		this(builder, new ModuleContext(), null);
 	}
 	
 	public JAXBOutputFactory(CityGMLBuilder builder, ModuleContext moduleContext) {
-		super(moduleContext);
-		this.builder = builder;
+		this(builder, moduleContext, null);
 	}
 	
 	public JAXBOutputFactory(CityGMLBuilder builder, SchemaHandler schemaHandler) {
-		super(schemaHandler);
-		this.builder = builder;
-	}
-	
-	public JAXBOutputFactory(CityGMLBuilder builder, ModuleContext moduleContext, SchemaHandler schemaHandler) {
-		super(moduleContext, schemaHandler);
-		this.builder = builder;
+		this(builder, new ModuleContext(), schemaHandler);
 	}
 	
 	public CityGMLWriter createCityGMLWriter(File file, ModuleContext moduleContext) throws CityGMLWriteException {
@@ -262,6 +289,184 @@ public class JAXBOutputFactory extends AbstractCityGMLOutputFactory {
 	
 	public CityModelWriter createCityModelWriter(Writer writer) throws CityGMLWriteException {
 		return createCityModelWriter(writer, moduleContext);
+	}
+	
+	public ModuleContext getModuleContext() {
+		return moduleContext;
+	}
+
+	public SchemaHandler getSchemaHandler() {
+		return schemaHandler;
+	}
+
+	public void setSchemaHandler(SchemaHandler schemaHandler) {
+		if (schemaHandler == null)
+			throw new IllegalArgumentException("schema handler may not be null.");
+
+		this.schemaHandler = schemaHandler;
+	}
+
+	public void setModuleContext(ModuleContext moduleContext) {
+		if (moduleContext == null)
+			throw new IllegalArgumentException("module context may not be null.");
+
+		this.moduleContext = moduleContext;
+	}
+
+	public void setCityGMLVersion(CityGMLVersion version) {
+		if (version == null)
+			throw new IllegalArgumentException("CityGML version may not be null.");
+
+		moduleContext = new ModuleContext(version);
+	}
+
+	public GMLIdManager getGMLIdManager() {
+		return gmlIdManager;
+	}
+
+	public void setGMLIdManager(GMLIdManager gmlIdManager) {
+		if (gmlIdManager == null)
+			throw new IllegalArgumentException("gml:id manager may not be null.");
+
+		this.gmlIdManager = gmlIdManager;
+	}
+
+	public ValidationEventHandler getValidationEventHandler() {
+		return validationEventHandler;
+	}
+
+	public void setValidationEventHandler(ValidationEventHandler validationEventHandler) {
+		if (validationEventHandler == null)
+			throw new IllegalArgumentException("validation event handler may not be null.");
+
+		this.validationEventHandler = validationEventHandler;
+	}
+
+	public void setTransformationTemplates(Templates... transformationTemplates) throws CityGMLWriteException {
+		if (transformationTemplates == null)
+			throw new IllegalArgumentException("transformation templates may not be null.");
+
+		try {
+			if (transformerChainFactory == null)
+				transformerChainFactory = new TransformerChainFactory(transformationTemplates);
+			else
+				transformerChainFactory.updateTemplates(transformationTemplates);
+		} catch (TransformerConfigurationException e) {
+			throw new CityGMLWriteException("Caused by: ", e);
+		}
+	}
+
+	public Templates[] getTransformationTemplates() {
+		return transformerChainFactory == null ? null : transformerChainFactory.getTemplates();
+	}
+
+	public TransformerChainFactory getTransformerChainFactory() {
+		return transformerChainFactory;
+	}
+
+	public Object getProperty(String name) {
+		if (name == null)
+			throw new IllegalArgumentException("name may not be null.");
+
+		if (name.equals(CityGMLOutputFactory.FEATURE_WRITE_MODE))
+			return featureWriteMode;
+		if (name.equals(CityGMLOutputFactory.KEEP_INLINE_APPEARANCE))
+			return keepInlineAppearance;
+		if (name.equals(CityGMLOutputFactory.SPLIT_COPY))
+			return splitCopy;
+		if (name.equals(CityGMLOutputFactory.USE_VALIDATION))
+			return useValidation;
+		if (name.equals(CityGMLOutputFactory.EXCLUDE_FROM_SPLITTING))
+			return excludes;
+
+		throw new IllegalArgumentException("the property '" + name + "' is not supported.");
+	}
+
+	@SuppressWarnings("unchecked")
+	public void setProperty(String name, Object value) {
+		if (name == null)
+			throw new IllegalArgumentException("name may not be null.");
+
+		if (name.equals(CityGMLOutputFactory.FEATURE_WRITE_MODE)) {
+			FeatureWriteMode mode = FeatureWriteMode.fromValue(value.toString());
+			if (mode != null)
+				featureWriteMode = mode;
+
+			return;
+		}
+
+		if (name.equals(CityGMLOutputFactory.KEEP_INLINE_APPEARANCE)) {
+			if (value instanceof Boolean)
+				keepInlineAppearance = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		if (name.equals(CityGMLOutputFactory.SPLIT_COPY)) {
+			if (value instanceof Boolean)
+				splitCopy = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		if (name.equals(CityGMLOutputFactory.USE_VALIDATION)) {
+			if (value instanceof Boolean)
+				useValidation = ((Boolean)value).booleanValue();
+
+			return;		
+		}
+
+		if (name.equals(CityGMLOutputFactory.EXCLUDE_FROM_SPLITTING)) {
+			if (value instanceof Class<?>) {
+				if (isSubclassOfCityGML((Class<?>)value))
+					excludes.add((Class<? extends CityGML>)value);
+				else
+					throw new IllegalArgumentException("exclude must be of type Class<? extends CityGML>.");
+
+				return;
+			}
+
+			else if (value instanceof Collection<?>) {
+				for (Object o : (Collection<?>)value) {
+					if (isSubclassOfCityGML((Class<?>)o))
+						excludes.add((Class<? extends CityGML>)o);
+					else
+						throw new IllegalArgumentException("exclude must be of type Class<? extends CityGML>.");
+				}
+
+				return;
+			}
+
+			else if (value instanceof Class<?>[]) {
+				for (Object o : (Class<?>[])value) {
+					if (isSubclassOfCityGML((Class<?>)o))
+						excludes.add((Class<? extends CityGML>)o);
+					else
+						throw new IllegalArgumentException("exclude must be of type Class<? extends CityGML>.");
+				}
+
+				return;
+			}
+		}
+
+		throw new IllegalArgumentException("the key-value pair '" + name + " - " + value.getClass().getName() + "' is not supported.");
+	}
+
+	private boolean isSubclassOfCityGML(Class<?> a) {
+		if (a == null)
+			return false;
+
+		if (a == CityGML.class)
+			return true;
+
+		for (Class<?> tmp : a.getInterfaces())
+			if (isSubclassOfCityGML(tmp))
+				return true;
+
+		if (a.getSuperclass() != Object.class)
+			return isSubclassOfCityGML(a.getSuperclass());
+
+		return false;
 	}
 	
 }
