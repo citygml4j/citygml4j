6aef00031fbf99b42273f0e1c44c48b7a6d098b9
Claus Nagel <cnagel@virtualcitysystems.de>
2018-03-24 13:35:08 +0100

    updated examples to CityJSON 0.6

--- citygml4j-samples/src/main/java/cityjson/citygml2cityjson/ChunkConverter.java
+++ citygml4j-samples/src/main/java/cityjson/citygml2cityjson/ChunkConverter.java
@@ -40,7 +40,7 @@ public class ChunkConverter {
 		
 		/*
 		 * This example demonstrates how to write a CityJSON file chunk-wise.
-		 * Note that the chunk-wise approach faces some limitations in CityJSON v0.5:
+		 * Note that the chunk-wise approach faces some limitations in CityJSON v0.6:
 		 * 
 		 * Firstly, some CityJSON elements like the "vertices" and "vertices-texture"
 		 * arrays are global. Thus, even if we write city objects chunk-wise, these
