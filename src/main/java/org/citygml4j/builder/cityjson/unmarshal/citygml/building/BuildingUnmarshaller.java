0119632278a25607d91b65bde4326cee4ba58cd5
Claus Nagel <cnagel@virtualcitysystems.de>
2018-10-03 13:19:06 +0200

    fixed bug in CityJSON BuildingUnmarshaller

--- src/main/java/org/citygml4j/builder/cityjson/unmarshal/citygml/building/BuildingUnmarshaller.java
+++ src/main/java/org/citygml4j/builder/cityjson/unmarshal/citygml/building/BuildingUnmarshaller.java
@@ -213,7 +213,7 @@ public class BuildingUnmarshaller {
 			if (geometryType instanceof AbstractGeometryObjectType) {
 				AbstractGeometryObjectType geometryObject = (AbstractGeometryObjectType) geometryType;
 				geometry = json.getGMLUnmarshaller().unmarshal(geometryObject, dest);
-				geometryObject.getLod().intValue();
+				lod = geometryObject.getLod().intValue();
 			} else if (geometryType instanceof GeometryInstanceType) {
 				GeometryInstanceType geometryInstance = (GeometryInstanceType) geometryType;
 				geometry = citygml.getCoreUnmarshaller().unmarshalAndTransformGeometryInstance(geometryInstance, dest);
