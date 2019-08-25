658c1fb6010bd973de5b6e186d463d1bbdc91cb3
Claus Nagel <cnagel@virtualcitysystems.de>
2019-08-25 13:43:56 +0200

    minor change

--- src/main/java/org/citygml4j/adapter/xml/building/BuildingAdapter.java
+++ src/main/java/org/citygml4j/adapter/xml/building/BuildingAdapter.java
@@ -1,14 +1,11 @@
 package org.citygml4j.adapter.xml.building;
 
 import org.citygml4j.adapter.xml.CityGMLSerializerHelper;
-import org.citygml4j.adapter.xml.core.AbstractCityObjectAdapter;
 import org.citygml4j.adapter.xml.core.AbstractSpaceAdapter;
 import org.citygml4j.model.building.Building;
-import org.citygml4j.model.core.AbstractSpace;
 import org.citygml4j.util.CityGMLConstants;
 import org.xmlobjects.annotation.XMLElement;
 import org.xmlobjects.annotation.XMLElements;
-import org.xmlobjects.gml.adapter.geometry.primitives.AbstractSurfaceAdapter;
 import org.xmlobjects.serializer.ObjectSerializeException;
 import org.xmlobjects.xml.Element;
 import org.xmlobjects.xml.Namespaces;
