a975ee8d75dcb49577ddd8dca77fa43276bc197c
Claus Nagel <cnagel@virtualcitysystems.de>
2019-11-07 19:42:21 +0100

    minor change

--- src/main/java/org/citygml4j/model/CityGMLObject.java
+++ src/main/java/org/citygml4j/model/CityGMLObject.java
@@ -1,6 +1,7 @@
 package org.citygml4j.model;
 
 import org.xmlobjects.model.Child;
+import org.xmlobjects.util.copy.Copyable;
 
-public interface CityGMLObject extends Child {
+public interface CityGMLObject extends Child, Copyable {
 }
