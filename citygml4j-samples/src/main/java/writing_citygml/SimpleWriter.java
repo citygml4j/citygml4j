d62c22a15f6e870f1f7bf98404d371c7a0fb9a50
Claus Nagel <cnagel@virtualcitysystems.de>
2020-04-08 16:07:45 +0200

    added SimpleWriter sample program

---
+++ citygml4j-samples/src/main/java/writing_citygml/SimpleWriter.java
@@ -0,0 +1,70 @@
+/*
+ * citygml4j - The Open Source Java API for CityGML
+ * https://github.com/citygml4j
+ *
+ * Copyright 2013-2020 Claus Nagel <claus.nagel@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package writing_citygml;
+
+import org.citygml4j.CityGMLContext;
+import org.citygml4j.model.CityGMLVersion;
+import org.citygml4j.model.core.AbstractFeature;
+import org.citygml4j.xml.module.citygml.CoreModule;
+import org.citygml4j.xml.reader.CityGMLInputFactory;
+import org.citygml4j.xml.reader.CityGMLReader;
+import org.citygml4j.xml.writer.CityGMLOutputFactory;
+import org.citygml4j.xml.writer.CityGMLWriter;
+import util.Logger;
+import util.Util;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+public class SimpleWriter {
+
+    public static void main(String[] args) throws Exception {
+        Logger log = Logger.start(SimpleWriter.class);
+
+        CityGMLContext context = CityGMLContext.newInstance();
+
+        CityGMLInputFactory in = context.createCityGMLInputFactory();
+
+        Path file = Util.SAMPLE_DATA_DIR.resolve("lod3_building_v2.gml");
+        log.print("Reading the file " + file + " into main memory");
+
+        AbstractFeature feature;
+        try (CityGMLReader reader = in.createCityGMLReader(file)) {
+            feature = reader.next();
+            log.print("Found " + reader.getName().getLocalPart() + " with gml:id " + feature.getId());
+        }
+
+        CityGMLVersion version = CityGMLVersion.v2_0;
+        CityGMLOutputFactory out = context.createCityGMLOutputFactory(version);
+
+        Path output = Util.getOutputFile();
+        log.print("Writing the feature as CityGML " + version + " file " + output);
+
+        try (CityGMLWriter writer = out.createCityGMLWriter(output, StandardCharsets.UTF_8.name())) {
+            writer.withIndentString("  ")
+                    .withDefaultSchemaLocations()
+                    .withDefaultPrefixes()
+                    .withDefaultNamespace(CoreModule.of(version).getNamespaceURI())
+                    .write(feature);
+        }
+
+        log.finish();
+    }
+}
