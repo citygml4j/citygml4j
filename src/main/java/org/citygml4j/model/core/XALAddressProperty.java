36c74dc308f412f97c99e56e0d9f6eb5977c8655
Claus Nagel <cnagel@virtualcitysystems.de>
2020-07-12 12:57:05 +0200

    xAL 3.0 Address is now used in core module

--- src/main/java/org/citygml4j/model/core/XALAddressProperty.java
+++ src/main/java/org/citygml4j/model/core/XALAddressProperty.java
@@ -2,19 +2,18 @@ package org.citygml4j.model.core;
 
 import org.citygml4j.model.CityGMLObject;
 import org.xmlobjects.gml.model.base.AbstractInlineProperty;
-import org.xmlobjects.xal.model.AddressDetails;
 
-public class XALAddressProperty extends AbstractInlineProperty<AddressDetails> implements CityGMLObject {
+public class XALAddressProperty extends AbstractInlineProperty<Address> implements CityGMLObject {
 
     public XALAddressProperty() {
     }
 
-    public XALAddressProperty(AddressDetails object) {
+    public XALAddressProperty(Address object) {
         super(object);
     }
 
     @Override
-    public Class<AddressDetails> getTargetType() {
-        return AddressDetails.class;
+    public Class<Address> getTargetType() {
+        return Address.class;
     }
 }
