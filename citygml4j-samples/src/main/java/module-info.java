1ff36984d7515c78cf4108b7370be7d1c102a535
Claus Nagel <cnagel@virtualcitysystems.de>
2021-10-25 21:38:20 +0200

    added TestADEContext provider to module-info

--- citygml4j-samples/src/main/java/module-info.java
+++ citygml4j-samples/src/main/java/module-info.java
@@ -5,8 +5,9 @@ module org.citygml4j.samples {
     exports implementing_ades.adapter to org.xmlobjects;
     exports implementing_ades.walker to org.citygml4j;
 
+    uses org.citygml4j.xml.ade.ADEContext;
+    provides org.citygml4j.xml.ade.ADEContext with implementing_ades.TestADEContext;
+
     opens implementing_ades.model to org.citygml4j;
     opens processing_citygml to org.citygml4j;
-
-    uses org.citygml4j.xml.ade.ADEContext;
 }
