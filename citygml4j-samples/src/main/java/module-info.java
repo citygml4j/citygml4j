0d3f6725cf8d806eddca9c23225a60b651d171a2
Claus Nagel <cnagel@virtualcitysystems.de>
2021-10-25 20:06:14 +0200

    turned citygml4j samples into a separate Java module

---
+++ citygml4j-samples/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.citygml4j.samples {
+    requires org.citygml4j;
+
+    exports implementing_ades.model;
+    exports implementing_ades.adapter to org.xmlobjects;
+    exports implementing_ades.walker to org.citygml4j;
+
+    opens implementing_ades.model to org.citygml4j;
+    opens processing_citygml to org.citygml4j;
+
+    uses org.citygml4j.xml.ade.ADEContext;
+}
