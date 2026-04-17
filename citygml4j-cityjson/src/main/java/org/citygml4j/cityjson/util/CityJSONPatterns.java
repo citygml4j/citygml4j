5d2b2fb7d1819fb7f9ad5e5a916cbc121a8e6776
Claus Nagel <cnagel@virtualcitysystems.de>
2026-04-17 14:14:57 +0200

    allow CityJSON extension versions to be X.Y.Z instead of just X.Y

--- citygml4j-cityjson/src/main/java/org/citygml4j/cityjson/util/CityJSONPatterns.java
+++ citygml4j-cityjson/src/main/java/org/citygml4j/cityjson/util/CityJSONPatterns.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 
 public class CityJSONPatterns {
     public static final Pattern LOD = Pattern.compile("^(\\d\\.)?(\\d)$");
-    public static final Pattern EXTENSION_VERSION = Pattern.compile("^(\\d+\\.)(\\d)+$");
+    public static final Pattern EXTENSION_VERSION = Pattern.compile("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*))?$");
     public static final Pattern CRS_URL = Pattern.compile("^https?://www.opengis.net/def/crs/(.+?)/(.*?)/(\\d+)$");
     public static final Pattern CRS_URN = Pattern.compile("^urn:ogc:def:crs(?:,crs)?:(.+?):(.*?):(\\d+).*");
     public static final Pattern CRS_SHORT = Pattern.compile("^([^:]+):(\\d+)$");
